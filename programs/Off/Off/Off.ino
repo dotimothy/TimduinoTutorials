df5fa82611196e35b59796f344f30dd3f18db80b
Timothy Do <timothydobsa@gmail.com>
2020-09-12 00:09:19 -0700

    Added Arduino Programs

---
+++ programs/Off/Off/Off.ino
@@ -0,0 +1,12 @@
+//turns arduino off
+int led = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, LOW);
+
+}
