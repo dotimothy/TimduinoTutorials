865606d523face02ebbee239357219658650b9f1
Timothy Do <timothydobsa@gmail.com>
2020-09-12 02:08:04 -0700

    Added indicator light into the effect

--- Programs/Halloween/Halloween.ino
+++ Programs/Halloween/Halloween.ino
@@ -1,5 +1,5 @@
 /*
- * Halloween.ino: A program to control leds with an eerie strobing effect
+ * Halloween.ino: A program to control leds and indicator with an eerie strobing effect
  * Required Components
  * 1 Arduino Uno
  * 1 Breadboard
@@ -10,15 +10,19 @@
 
 //connect positve to pin 6, negative to ground
 int led = 6;
+int indicate = 13;
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(indicate, OUTPUT);
 }
 
 void loop() {
   //turning the led on 
   analogWrite(led, 255);
+  digitalWrite(indicate,LOW);
   delay(50);
   //turning the led off
   analogWrite(led, 0);
+  digitalWrite(indicate,HIGH);
   delay(50);
 }
