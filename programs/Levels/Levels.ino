8eecef41bbe1409e60028ec6ef3ae5a33f2f8c58
Timothy Do <timothydobsa@gmail.com>
2020-09-12 22:23:07 -0700

    added so much

--- programs/Levels/Levels.ino
+++ programs/Levels/Levels.ino
@@ -1,17 +1,31 @@
+/*
+ * Levels.ino: Goes in between 3 levels of brightness for LED in Pin 6
+ * Required Components:
+ * 1 Arduino Uno
+ * 1 Breadboard
+ * 1 1K Ω Resistor
+ */
 
+ //setting led variable to pin 6
 int led = 6;
 void setup() {
+  //setting outputs
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  //bright
   analogWrite(led, 255); 
   delay(1000);
+  //dimmer
   analogWrite(led, 32);
   delay(1000);
+  //off
   analogWrite(led,0); 
   delay(1000);
+  //dimmer
   analogWrite(led,32);
   delay(1000);
+  //bright
   analogWrite(led,255);
 }
