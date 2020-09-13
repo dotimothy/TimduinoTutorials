e04b36d7de7abacac588a155a33d37a3b21ce666
Timothy Do <timothydobsa@gmail.com>
2020-09-12 22:11:46 -0700

    added lowercase

---
+++ programs/On/On.ino
@@ -0,0 +1,17 @@
+/*
+  On.ino: Turns on the pin 13 (led indicator) on the Arduino Uno
+  Required Components
+  Arduino Uno
+*/
+
+//setting led pin to pin 13
+int led = 13;
+void setup() {
+  // setting outputs
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  //setting pin 13 to HIGH (thus turning it on)
+  digitalWrite(led, HIGH);
+}
