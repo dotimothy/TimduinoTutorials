26eb0bbcabd4756cbd4b20d4dd0e5b13e9dbbf99
Timothy Do <timothd4@uci.edu>
2022-10-27 19:36:48 -0700

    Update square.ino

--- programs/square/square.ino
+++ programs/square/square.ino
@@ -2,14 +2,18 @@ int pin = 10;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin,OUTPUT);
-
+  Serial.begin(9600);
 }
 
 //loop
 void loop() {
-  //digitalWrite(pin,HIGH);
-  pwm(pin,1.075*440);
-  //square(pin,440,50);
+  //pwm(pin,1.075*440);
+  for(int freq = 0; freq < 20000; freq++) {
+    square(pin,freq,50);
+  }
+   for(int freq = 20000; freq > 0; freq--) {
+    square(pin,freq,50);
+  }
 }
 
 void pwm(int pin, float frequency) {
